82ff34b6e926d8f808ff485d9bfdd330e7214e8c
otienochris <otienochris98@gmail.com>
2023-05-26 17:25:30 +0300

    initial commit

---
+++ src/main/java/com/example/frontendscreensservice/FrontendScreensServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.frontendscreensservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendScreensServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendScreensServiceApplication.class, args);
+    }
+
+}
